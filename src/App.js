6a6bc0f4dd8283fda30a997ab9afb2af4e793a65
Serge <malashta@gmail.com>
2020-11-10 10:21:48 +0700

    add navigation and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Navbar} from "./components/Navbar";
 
 function App() {
   return (
-    <div className="container pt-4">
-      <h1>test</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container pt-4">
+        <h1>test</h1>
+      </div>
+    </>
   );
 }
 
