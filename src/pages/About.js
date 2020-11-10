453abfcbe2109a27c4450b110ea835e1becb3686
Serge <malashta@gmail.com>
2020-11-10 10:55:54 +0700

    add about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,12 @@ import React from 'react'
 export const About = () => {
   return (
     <div>
-      <h1>About page</h1>
+      <div className="jumbotron">
+        <div className="container">
+          <h1 className="display-4">Информация о приложении</h1>
+          <p className="lead">Версия 1.0.0</p>
+        </div>
+      </div>
     </div>
   )
 }
