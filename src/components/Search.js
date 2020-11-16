51192e28000d7347590ba69d111d0e7a8e9203be
Serge <malashta@gmail.com>
2020-11-16 09:02:11 +0700

    add github requests

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
+import {AlertContext} from "../context/alert/alertContext";
+import {GithubContext} from "../context/github/githubContext";
 
 export const Search = () => {
+  const [value, setValue] = useState('');
+  const {show} = useContext(AlertContext);
+  const github = useContext(GithubContext);
+
+  const onSubmit = event => {
+    if (event.key !== 'Enter') {
+      return true;
+    }
+
+    if (value.trim()) {
+      github.search(value.trim());
+    } else {
+      show('введите данные пользователя');
+    }
+  }
+
   return (
     <div className="form-group">
       <input
         type="text"
         className="form-control"
         placeholder="Введите имя пользователя..."
+        onKeyPress={onSubmit}
+        value={value}
+        onChange={event => setValue(event.target.value)}
       />
     </div>
   )
